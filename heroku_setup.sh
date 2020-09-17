b0ef707cd49539ed64cd2a3db89bc5c80ec1ebf2
Nikolas Rieble <nikolas-rieble@gmx.de>
2020-09-17 19:29:37 +0200

    Implement script to deploy from zero (#1)

---
+++ heroku_setup.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+function setupHeroku {
+
+    local RandomNamePart
+    RandomNamePart=$(openssl rand -hex 8)
+    heroku create "airflow-$RandomNamePart"
+
+    heroku addons:create heroku-postgresql:hobby-dev
+
+    local DATABASE_CONN
+    DATABASE_CONN=$(heroku config:get DATABASE_URL)
+    heroku config:set AIRFLOW__CORE__SQL_ALCHEMY_CONN="$DATABASE_CONN"
+
+    heroku config:set AIRFLOW__CORE__LOAD_EXAMPLES=False
+    heroku config:set AIRFLOW_HOME=/app
+
+    local FERNET_KEY
+    FERNET_KEY=$(python -c "from cryptography.fernet import Fernet; print(Fernet.generate_key().decode())")
+    heroku config:set AIRFLOW__CORE__FERNET_KEY="$FERNET_KEY"
+
+    git push heroku master
+
+    heroku run "python initial_user_creation.py"
+
+    heroku run "rm initial_user_creation.py"
+}
+
+setupHeroku
+
+
+
