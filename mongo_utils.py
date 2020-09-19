6db3899e192b83d7b220e0490d769b55c365ebc4
Nikolas Rieble <nikolas.rieble@scalable.capital>
2020-09-19 07:38:30 +0200

    Add mongo-utils and test get_open_task() method

---
+++ mongo_utils.py
@@ -0,0 +1,25 @@
+import os
+import pymongo
+
+class MongoDb:
+
+    def __init__(self):
+        mongodb_string = os.environ.get('MONGO_DB')
+        self.myclient =  pymongo.MongoClient(mongodb_string)
+
+    def insert(self, data: dict, language):
+        mydb = self.myclient['newspaper']
+        collection = mydb[language]
+        # prevent duplicates
+        if collection.count_documents({'headline': data["title"]}) == 0:
+            collection.insert_one(data)
+
+    def get_open_task(self):
+        database = self.myclient['TODO']
+        collection = database['TODO']
+        return collection.find_one({'scraped': 0})
+
+    def set_task_solved(self, task):
+        database = self.myclient['TODO']
+        collection = database['TODO']
+        return collection.update_one({'url': task['url']}, {'$set': {'scraped': 1}}, upsert=False)
---
+++ tests/test_DAG_url_scraper.py
@@ -0,0 +1,54 @@
+from unittest import TestCase
+
+import mongomock as mongomock
+
+from mongo_utils import MongoDb
+
+
+class Test(TestCase):
+
+    def test_MongoDB_get_open_task_returns_none_for_empty_collection(self):
+        # given
+        db = MongoDb()
+        db.myclient = mongomock.MongoClient()
+        # when
+        task = db.get_open_task()
+        # then
+        assert task == None
+
+    def test_MongoDB_get_open_task_returns_none_if_no_task_open(self):
+        # given
+        db = MongoDb()
+        db.myclient = mongomock.MongoClient()
+        db.myclient["TODO"]["TODO"].insert({"scraped": 1, "id": 1})
+        # when
+        task = db.get_open_task()
+        # then
+        assert task == None
+
+    def test_MongoDB_get_open_task_returns_open_task(self):
+        # given
+        db = MongoDb()
+        db.myclient = mongomock.MongoClient()
+
+        open_task = {"scraped": 0, "id": 1}
+        open_task['_id'] = db.myclient["TODO"]["TODO"].insert(open_task)
+        # when
+        task = db.get_open_task()
+        # then
+        assert task == open_task
+
+    def test_MockMongo(self):
+
+        def increase_votes(collection):
+            collection.update_many({}, {'$inc': {'votes': 1}})
+
+        collection = mongomock.MongoClient().db.collection
+        objects = [dict(votes=1), dict(votes=2)]
+        for obj in objects:
+            obj['_id'] = collection.insert_one(obj).inserted_id
+        increase_votes(collection)
+        for obj in objects:
+            stored_obj = collection.find_one({'_id': obj['_id']})
+            stored_obj['votes'] -= 1
+            assert stored_obj == obj  # by comparing all fields we make sure only votes changed
