c15e793f1289286b5c23e893350a1c6e748f01fd
Nikolas Rieble <nikolas.rieble@scalable.capital>
2020-09-20 12:41:22 +0200

    Test set_task_solved

--- mongo_utils.py
+++ mongo_utils.py
@@ -6,22 +6,21 @@ class MongoDb:
 
     def __init__(self):
         mongodb_string = os.environ.get('MONGO_DB')
-        self.myclient = pymongo.MongoClient(mongodb_string)
+        self._database = pymongo.MongoClient(mongodb_string)['newspaper']
 
     def insert(self, data: dict, language):
-        mydb = self.myclient['newspaper']
-        collection = mydb[language]
-        # prevent duplicates
+        collection = self._database[language]
         # TODO include other fields in duplicate check - duplicate title only should be allowed
         if collection.count_documents({'title': data["title"]}) == 0:
             collection.insert_one(data)
 
     def get_open_task(self):
-        database = self.myclient['TODO']
-        collection = database['TODO']
+        collection = self._get_task_collection()
         return collection.find_one({'scraped': 0})
 
     def set_task_solved(self, task):
-        database = self.myclient['TODO']
-        collection = database['TODO']
+        collection = self._get_task_collection()
         return collection.update_one({'url': task['url']}, {'$set': {'scraped': 1}}, upsert=False)
+
+    def _get_task_collection(self):
+        return self._database['TODO']
--- tests/test_mongo_utils.py
+++ tests/test_mongo_utils.py
@@ -9,59 +9,79 @@ class Test(TestCase):
 
     def test_MongoDB_get_open_task_returns_none_for_empty_collection(self):
         # given
-        db = MongoDb()
-        db.myclient = mongomock.MongoClient()
+        mongo_database = MongoDb()
+        mongo_database._database = mongomock.MongoClient()["newspaper"]
+
         # when
-        task = db.get_open_task()
+        task = mongo_database.get_open_task()
+
         # then
-        assert task == None
+        assert task is None
 
     def test_MongoDB_get_open_task_returns_none_if_no_task_open(self):
         # given
-        db = MongoDb()
-        db.myclient = mongomock.MongoClient()
-        db.myclient["TODO"]["TODO"].insert_one({"scraped": 1, "id": 1})
+        mongo_database = MongoDb()
+        mongo_database._database = mongomock.MongoClient()["newspaper"]
+        mongo_database._database["TODO"].insert_one({"scraped": 1, "id": 1})
+
         # when
-        task = db.get_open_task()
+        task = mongo_database.get_open_task()
+
         # then
-        assert task == None
+        assert task is None
 
     def test_MongoDB_get_open_task_returns_open_task(self):
         # given
-        db = MongoDb()
-        db.myclient = mongomock.MongoClient()
-
+        mongo_database = MongoDb()
+        mongo_database._database = mongomock.MongoClient()["newspaper"]
         open_task = {"scraped": 0, "id": 1}
-        open_task['_id'] = db.myclient["TODO"]["TODO"].insert_one(open_task).inserted_id
+        open_task['_id'] = mongo_database._database["TODO"].insert_one(open_task).inserted_id
+
         # when
-        task = db.get_open_task()
+        task = mongo_database.get_open_task()
+
         # then
         assert task == open_task
 
     def test_insert_into_empty_db(self):
         # given
-        db = MongoDb()
-        db.myclient = mongomock.MongoClient()
+        mongo_database = MongoDb()
+        mongo_database._database = mongomock.MongoClient()["newspaper"]
         expected_article = {"title": "How Millennials Are Disrupting Test"}
 
         # when
-        db.insert(expected_article, "LANGUAGE")
+        mongo_database.insert(expected_article, "LANGUAGE")
 
         # then
-        actual_article = db.myclient["newspaper"]["LANGUAGE"].find_one({})
+        actual_article = mongo_database._database["LANGUAGE"].find_one({})
         assert actual_article["title"] == expected_article["title"]
 
     def test_insert_does_not_create_duplicate(self):
         # given
-        db = MongoDb()
-        db.myclient = mongomock.MongoClient()
+        mongo_database = MongoDb()
+        mongo_database._database = mongomock.MongoClient()["newspaper"]
         existing_article = {"title": "How Millennials Are Disrupting Test"}
-        existing_article["_id"] = db.myclient["newspaper"]["LANGUAGE"].insert_one(existing_article).inserted_id
+        existing_article["_id"] = mongo_database._database["newspaper"]["LANGUAGE"].insert_one(
+            existing_article).inserted_id
 
         # when
-        db.insert(existing_article, "LANGUAGE")
+        mongo_database.insert(existing_article, "LANGUAGE")
 
         # then
-        article_count = db.myclient["newspaper"]["LANGUAGE"].count_documents({})
+        article_count = mongo_database._database["newspaper"]["LANGUAGE"].count_documents({})
         assert article_count == 1
 
+    def test_set_task_solved(self):
+        # given
+        mongo_database = MongoDb()
+        mongo_database._database = mongomock.MongoClient()["newspaper"]
+        open_task = {"scraped": 0, "url": "www.google.com"}
+        open_task['_id'] = mongo_database._database["TODO"].insert_one(open_task).inserted_id
+
+        # when
+        mongo_database.set_task_solved(open_task)
+
+        # then
+        actual_task = mongo_database._database["TODO"].find_one({"url": "www.google.com"})
+        assert actual_task["url"] == open_task["url"]
+        assert actual_task["scraped"] == 1
