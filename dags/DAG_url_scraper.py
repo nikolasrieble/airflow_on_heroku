93673b1cfb4efd69dda62211576af214809b4a3f
Nikolas Rieble <nikolas.rieble@scalable.capital>
2020-09-20 14:21:31 +0200

    Use MongoDb in DAG_url_scraper.py

--- dags/DAG_url_scraper.py
+++ dags/DAG_url_scraper.py
@@ -5,6 +5,8 @@ import newspaper
 from airflow.models import DAG
 from airflow.operators.python_operator import PythonOperator
 
+from mongo_utils import MongoDb
+
 default_args = {
     'owner': 'niko_huy',
     'start_date': datetime.datetime(2020, 2, 18),
@@ -24,8 +26,15 @@ input_list = {
 }
 
 
+def create_task(article, language):
+    return {'url': article.url,
+            'scraped': 0,
+            'language': language}
+
+
 def url_scraper(templates_dict, language, **context):
-    collection = get_collection(templates_dict)
+    database = MongoDb()
+
     newspaper_url = input_list.get(language)
 
     paper = newspaper.build(newspaper_url,
@@ -34,21 +43,8 @@ def url_scraper(templates_dict, language, **context):
                             fetch_images=False,
                             MIN_WORD_COUNT=100)
 
-    for article in paper.articles:
-        # prevent duplicates
-        if collection.count_documents({'url': article.url}) == 0:
-            collection.insert_one({'url': article.url,
-                                   'scraped': 0,
-                                   'language': language})
-
-
-def get_collection(templates_dict):
-    mongodb_string = templates_dict.get('mongodb_string')
-    assert mongodb_string
-    myclient = pymongo.MongoClient(mongodb_string)
-    mydb = myclient['TODO']
-    collection = mydb['TODO']
-    return collection
+    tasks = [create_task(article, language) for article in paper.articles]
+    database.insert_tasks(tasks)
 
 
 dag = DAG('url_scraper',
